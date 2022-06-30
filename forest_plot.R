75e422fc26536d05810df0a4e07982cc30239e73
rosebaesj <rosebae@khu.ac.kr>
2022-07-01 04:42:46 +0900

    save

--- forest_plot.R
+++ forest_plot.R
@@ -10,17 +10,45 @@ getwd()
 setwd("gitignore")
 
 ################ IMPORT DATA ###########
+
 metadata <- read.table("metadata.tsv", header = TRUE, sep = "\t")
-alpha_ACE <- read.table("input_alpha_div/alpha_ACE.tsv", header = TRUE, sep = "\t")
+
+
+#### + alpha diversity ####
+alpha_div <- c('a_ACE', 'a_Chao1' , 'a_OTU' , 'a_Shannon' , 'a_Simpson' , 'a_Sobs')
+
+a_ACE <- read.table("input_alpha_div/a_ACE.tsv", header = TRUE, sep = "\t")
+a_Chao1 <- read.table("input_alpha_div/a_Chao1.tsv", header = TRUE, sep = "\t")
+a_OTU <- read.table("input_alpha_div/a_OTU.tsv", header = TRUE, sep = "\t")
+a_Shannon <- read.table("input_alpha_div/a_Shannon.tsv", header = TRUE, sep = "\t")
+a_Simpson <- read.table("input_alpha_div/a_Simpson.tsv", header = TRUE, sep = "\t")
+a_Sobs <- read.table("input_alpha_div/a_Sobs.tsv", header = TRUE, sep = "\t")
+
+#### merge metadata to wanted data 
+
+
+
+#### + bugs ######
+
+P_Actinobacteria <- read.table("input_rela_abd/P_Actinobacteria.tsv", header = TRUE, sep = "\t")
+P_Bacteroidetes <- read.table("input_rela_abd/P_Bacteroidetes.tsv", header = TRUE, sep = "\t")
 P_Firmicutes <- read.table("input_rela_abd/P_Firmicutes.tsv", header = TRUE, sep = "\t")
+P_Proteobacteria <- read.table("input_rela_abd/P_Proteobacteria.tsv", header = TRUE, sep = "\t")
+P_Verrucomicrobia <- read.table("input_rela_abd/P_Verrucomicrobia.tsv", header = TRUE, sep = "\t")
+
+G_Bacteroides <- read.table("input_rela_abd/G_Bacteroides.tsv", header = TRUE, sep = "\t")
+G_Bifidobacterium <- read.table("input_rela_abd/G_Bifidobacterium.tsv", header = TRUE, sep = "\t")
+G_Escherichia <- read.table("input_rela_abd/G_Escherichia.tsv", header = TRUE, sep = "\t")
+G_Faecalibacterium <- read.table("input_rela_abd/G_Faecalibacterium.tsv", header = TRUE, sep = "\t")
+G_Lactobacillus <- read.table("input_rela_abd/G_Lactobacillus.tsv", header = TRUE, sep = "\t")
+G_Roseburia <- read.table("input_rela_abd/G_Roseburia.tsv", header = TRUE, sep = "\t")
+G_Ruminococcus <- read.table("input_rela_abd/G_Ruminococcus.tsv", header = TRUE, sep = "\t")
 
 
 
 
-#merge metadata to wanted data
-alpha_ACE <- left_join(alpha_ACE, metadata, by="id")
-P_Firmicutes <- left_join(P_Firmicutes, metadata, by="id")
-
+################ META ANALYSIS ###########
+###### + SMD #######
 #run meta analysis
 metacont_ACE <- metacont(i_num, i_mean, i_sd, c_num, c_mean, c_sd, 
          sm = "SMD", method.smd = "Hedges", id,
@@ -31,51 +59,195 @@ meta_P_Firmicutes <- metacont(i_num, i_mean, i_sd, c_num, c_mean, c_sd,
                          data = P_Firmicutes)
 
 
-######### ROM
-ROM_P_Firmicutes <- metacont(i_num, i_mean, i_sd, c_num, c_mean, c_sd, 
-                             sm = "ROM", id,
-                             #backtransf # TRUE (default), results will be presented as ratio of means; otherwise log ratio of means will be shown.
-                             data = P_Firmicutes)
-tROM_P_Firmicutes <- table_ROM(ROM_P_Firmicutes)
-check_plot(tROM_P_Firmicutes)
+######### + ROM ########
 
 
-ggplot(data = tROM_P_Firmicutes, aes(x=TE, y=studlab, color = signif, shape = signif)) +
+##### ++ apha diversity ######
+##grouped
+
+gtROM_ACE <- gtROM(a_ACE)
+gtROM_Chao1 <- gtROM(a_Chao1)
+gtROM_OTU <- gtROM(a_OTU)
+gtROM_Shannon <- gtROM(a_Shannon)
+gtROM_Simpson <- gtROM(a_Simpson)
+gtROM_Sobs <- gtROM(a_Sobs)
+
+gtROM_Actinobacteria <- gtROM(P_Actinobacteria)
+gtROM_Bacteroidetes <- gtROM(P_Bacteroidetes)
+gtROM_Firmicutes <- gtROM(P_Firmicutes)
+gtROM_Proteobacteria <- gtROM(P_Proteobacteria)
+gtROM_Verrucomicrobia <- gtROM(P_Verrucomicrobia)
+
+gtROM_Bacteroides <- gtROM(G_Bacteroides)
+gtROM_Bifidobacterium <- gtROM(G_Bifidobacterium)
+gtROM_Escherichia <- gtROM(G_Escherichia)
+gtROM_Faecalibacterium <- gtROM(G_Faecalibacterium)
+gtROM_Lactobacillus <- gtROM(G_Lactobacillus)
+gtROM_Roseburia <- gtROM(G_Roseburia)
+gtROM_Ruminococcus <- gtROM(G_Ruminococcus)
+
+##when subgroup analysis is meaningless
+
+ROM_Sobs <- metacont(i_num, i_mean, i_sd, c_num, c_mean, c_sd, 
+                      sm = "ROM", id,  data = a_Sobs)
+tROM_Sobs <- table_ROM(ROM_Sobs)
+
+
+
+######## DRAW PLOT ##########3
+
+
+#+ e skyblue3 / c pink3 / ns gray44
+#+ na 1 1.5 / stat 16 2 / sum 18 3 / 
+
+check_plot(gtROM_ACE)
+ggsave("output_alpha/gtROM_ACE.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Chao1)
+ggsave("output_alpha/gtROM_Chao1.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_OTU)
+ggsave("output_alpha/gtROM_OTU.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Shannon)
+ggsave("output_alpha/gtROM_Shannon.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Simpson)
+ggsave("output_alpha/gtROM_Simpson.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Sobs)
+ggsave("output_alpha/gtROM_Sobs.png", device = "png", width = 4, height = 4, units = "in")
+
+
+check_plot(gtROM_Actinobacteria)
+ggsave("output_alpha/gtROM_Actinobacteria.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Bacteroidetes)
+ggsave("output_alpha/gtROM_Bacteroidetes.png", device = "png", width = 4, height = 4, units = "in")
+
+check_plot(gtROM_Firmicutes)
+ggplot(data = gtROM_Firmicutes, aes(x=TE, y=studlab, color = signif, shape = stat)) +
+  ggtitle(deparse(substitute(gtROM_Firmicutes)))+
   geom_vline(xintercept = 0, linetype="dashed", color="grey50") +
-  geom_errorbarh(aes(xmin=lower, xmax=upper, alpha=!is.na(pval)), height=0 ) +
-  theme_MicrobeR() +
-  scale_color_manual(values=c("skyblue3", "pink3", "skyblue3","pink3", "skyblue3","pink3"))  +
-  geom_point(aes(size = signif))+
-  scale_shape_manual(values=c(1, 1, 16, 16, 18, 18))+
-  scale_size_manual(values=c(1.5, 1.5, 1.5, 1.5, 3, 3))+
+  geom_errorbarh(aes(xmin=lower, xmax=upper), height=0 ) +
+  scale_color_manual(values=c("skyblue3", "pink3"))  +
+  geom_point(aes(size = stat))+
+  scale_shape_manual(values=c(18, 5, 16, 1))+
+  scale_size_manual(values=c(2.5, 2, 1, 0.5))+
   scale_y_discrete(limits=rev)+
   theme(panel.border = element_blank(), axis.line = element_line()) +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
-ggsave("output/rela_abd/P_Firmicutes.png", device = "png", width = 3, height = 3, units = "in")
+  theme(axis.text.x=element_text(hjust=1))
+ggsave("output_alpha/gtROM_Firmicutes.png", device = "png", width = 4, height = 4, units = "in")
+
+check_plot(gtROM_Proteobacteria)
+ggsave("output_alpha/gtROM_Proteobacteria.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Verrucomicrobia)
+ggsave("output_alpha/gtROM_Verrucomicrobia.png", device = "png", width = 4, height = 4, units = "in")
+
+
+check_plot(gtROM_Bacteroides)
+ggsave("output_alpha/gtROM_Bacteroides.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Bifidobacterium)
+ggsave("output_alpha/gtROM_Bifidobacterium.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Escherichia)
+ggsave("output_alpha/gtROM_Escherichia.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Faecalibacterium)
+ggsave("output_alpha/gtROM_Faecalibacterium.png", device = "png", width = 4, height = 4, units = "in")
+
+check_plot(gtROM_Lactobacillus)
+ggplot(data = gtROM_Lactobacillus, aes(x=TE, y=studlab, color = signif, shape = stat)) +
+  ggtitle(deparse(substitute(gtROM_Lactobacillus)))+
+  geom_vline(xintercept = 0, linetype="dashed", color="grey50") +
+  geom_errorbarh(aes(xmin=lower, xmax=upper), height=0 ) +
+  scale_color_manual(values=c("skyblue3", "pink3"))  +
+  geom_point(aes(size = stat))+
+  scale_shape_manual(values=c(18, 5, 16, 1))+
+  scale_size_manual(values=c(2.5, 2, 1, 0.5))+
+  scale_y_discrete(limits=rev)+
+  theme(panel.border = element_blank(), axis.line = element_line()) +
+  theme(axis.text.x=element_text(hjust=1))
+ggsave("output_alpha/gtROM_Lactobacillus.png", device = "png", width = 4, height = 4, units = "in")
+
+check_plot(gtROM_Roseburia)
+ggsave("output_alpha/gtROM_Roseburia.png", device = "png", width = 4, height = 4, units = "in")
+check_plot(gtROM_Ruminococcus)
+ggsave("output_alpha/gtROM_Ruminococcus.png", device = "png", width = 4, height = 4, units = "in")
+
+
+
+check_plot(tROM_ACE)
+check_plot(gtROM_ACE)
+
+ggsave("output_alpha/forest_ACE.png", device = "png", width = 4, height = 4, units = "in")
+
+
+check_plot(tROM_Chao1)
+check_plot(tROM_Chao1)
+
+ggsave("output_alpha/forest_Chao1.png", device = "png", width = 3, height = 3, units = "in")
+
+
+
+check_plot(tROM_OTU)
+
+ggsave("output_alpha/forest_OTU.png", device = "png", width = 3, height = 3, #5로 나눈 정도
+       units = "in")
+
+
+
+check_plot(tROM_Shannon)
+
+ggsave("output_alpha/forest_Shannon.png", device = "png", width = 3, height = 3, #5로 나눈 정도
+       units = "in")
+
+check_plot(tROM_Simpson)
+
+ggsave("output_alpha/forest_Simpson.png", device = "png", width = 3, height = 3, #5로 나눈 정도
+       units = "in")
+
+
+
+
+check_plot(tROM_Sobs)
+
+ggsave("output_alpha/forest_Sobs.png", device = "png", width = 3, height = 3, #5로 나눈 정도
+       units = "in")
 
 
 
 
 
 
-ROM_alpha_ACE <- metacont(i_num, i_mean, i_sd, c_num, c_mean, c_sd, 
-                             sm = "ROM", id,
-                             #backtransf # TRUE (default), results will be presented as ratio of means; otherwise log ratio of means will be shown.
-                             data = alpha_ACE)
-tROM_alpha_ACE <- table_ROM(ROM_alpha_ACE)
 
-check_plot(tROM_alpha_ACE)
-ggplot(data = tROM_alpha_ACE, aes(x=TE, y=studlab, color = signif, shape = signif)) +
+
+ggplot(data = tROM_ACE, aes(x=TE, y=studlab, color = signif, shape = stat)) +
   geom_vline(xintercept = 0, linetype="dashed", color="grey50") +
   geom_errorbarh(aes(xmin=lower, xmax=upper), height=0 ) +
   theme_MicrobeR() +
-  scale_color_manual(values=c("skyblue3", "pink3", "gray44", "skyblue3","gray44"))  +
-  geom_point()+
-  scale_shape_manual(values=c(16, 16, 16, 18, 18))+
+  scale_color_manual(values=c("skyblue3", "pink3", "gray44"))  +
+  geom_point(aes(size = stat))+
+  scale_shape_manual(values=c(16, 18))+
+  scale_size_manual(values=c(1.5, 2))+
   scale_y_discrete(limits=rev)+
   theme(panel.border = element_blank(), axis.line = element_line()) +
   theme(axis.text.x=element_text(angle=45, hjust=1))
-ggsave("output/alpha/forest_ACE.png", device = "png", width = 3, height = 3, units = "in")
+
+
+
+
+
+ggplot(data = tROM_Sobs, aes(x=TE, y=studlab, color = signif, shape = stat)) +
+  geom_vline(xintercept = 0, linetype="dashed", color="grey50") +
+  geom_errorbarh(aes(xmin=lower, xmax=upper), height=0 ) +
+  theme_MicrobeR() +
+  scale_color_manual(values=c( "gray44"))  +
+  geom_point(aes(size = stat))+
+  scale_shape_manual(values=c(16, 18))+
+  scale_size_manual(values=c(1.5, 2))+
+  scale_y_discrete(limits=rev)+
+  theme(panel.border = element_blank(), axis.line = element_line()) +
+  theme(axis.text.x=element_text(angle=45, hjust=1))
+
+
+
+
+
+
+
 
 
 
--- functions.R
+++ functions.R
@@ -1,4 +1,15 @@
 ###### functions #####
+theme_set(
+  theme_bw()+
+    theme(axis.line = element_line(size=1),
+          axis.ticks = element_line(size=1),
+          panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          plot.title = element_text(hjust = 0.5, face="bold"),
+          plot.subtitle = element_text(hjust = 0.5)
+    ))
+
 ## SMD results are $TE
 
 table_SMD <- function(SMD){
@@ -44,7 +55,6 @@ table_SMD <- function(SMD){
 
 
 
-
 table_ROM <- function(ROM){
 
   df <- as.data.frame(ROM)
@@ -57,38 +67,162 @@ table_ROM <- function(ROM){
   colnames(random) <- colnames(m)
   
   if (fixed$pval>=0.05) {
-    fixed$signif <- "sum.ns"
+    fixed$stat <- "sum"    
+    fixed$signif <- "ns"
   } else {
-    fixed$signif <- ifelse(fixed$TE<0, "sum.e", "sum.c")
+    fixed$stat <- "sum"
+    fixed$signif <- ifelse(fixed$TE<0, "e", "c")
   }
   
   if (random$pval>=0.05) {
-    random$signif <- "sum.ns"
+    random$stat <- "sum"    
+    random$signif <- "ns"
   } else {
-    random$signif <- ifelse(random$TE<0, "sum.e", "sum.c")
+    random$stat <- "sum"
+    random$signif <- ifelse(random$TE<0, "e", "c")
   }
   
   signif <- NULL
+  stat <- NULL
   
   for (i in 1:nrow(m)) {
     if (is.na(m$pval[i])){
-      signif <- rbind(signif, ifelse(m$TE[i]<0, "na.e", "na.c"))
+      stat <- rbind(stat,"na")
+      signif <- rbind(signif, ifelse(m$TE[i]<0, "e", "c"))
     } else if (m$pval[i]>=0.05) {
-      signif <- rbind(signif, "stat.ns")
+      stat <- rbind(stat, "stat")
+      signif <- rbind(signif, "ns")
     } else {
-      signif <- rbind(signif, ifelse(m$TE[i]<0, "stat.e", "stat.c"))
+      stat <- rbind(stat, "stat")
+      signif <- rbind(signif, ifelse(m$TE[i]<0, "e", "c"))
     }
   }
 
- 
+  m$stat <- stat
   m$signif <- signif
+  
   m <- rbind(m, fixed, random)
-  m$signif <- factor(m$signif, levels = c("na.e", "na.c", "na.ns",
-                                      "stat.e", "stat.c", "stat.ns",
-                                      "sum.e", "sum.c", "sum.ns"), order=TRUE)
+  m$stat <- factor(m$stat, levels = c('na', 'stat', 'sum'), order = TRUE)
+  m$signif <- factor(m$signif, levels = c('e', 'c', 'ns'), order = TRUE)
+  
+  # m$signif <- factor(m$signif, levels = c("na.e", "na.c", "na.ns",
+  #                                     "stat.e", "stat.c", "stat.ns",
+  #                                     "sum.e", "sum.c", "sum.ns"), order=TRUE)
   m$studlab <- factor(m$studlab, levels = m$studlab, order=TRUE)
   return(m)
 }
+
+
+### grouped table
+
+gtROM <- function (aPG) {
+  aPG <- left_join(aPG, metadata, by="id")
+  gROM <- metacont(i_num, i_mean, i_sd, c_num, c_mean, c_sd, 
+                   sm = "ROM", id, subgroup = disease,
+                   #backtransf # TRUE (default), results will be presented as ratio of means; otherwise log ratio of means will be shown.
+                   data = aPG)
+  gtROM <- gtable_ROM(gROM)
+  return(gtROM)
+}
+
+
+gtable_ROM <- function(gROM){
+
+  df <- as.data.frame(gROM)
+  m <- df[,7:(ncol(df)-1)]
+  
+  #difference between fixed and random effect
+  
+  fixed <- data.frame(c("Fixed_Effect", gROM[c("TE.fixed", "seTE.fixed", "lower.fixed", "upper.fixed", "statistic.fixed", "pval.fixed", "zval.fixed")], 
+                        NA,NA, "FE", "sum"))
+  random <- data.frame(c("Random_Effect", gROM[c("TE.random", "seTE.random", "lower.random", "upper.random", "statistic.random", "pval.random", "zval.random")], 
+                         NA,NA, "RE", "sum"))
+  
+  gf <- data.frame(c(gROM[c("bylevs", "TE.fixed.w", "seTE.fixed.w", 
+                                "lower.fixed.w", "upper.fixed.w", "statistic.fixed.w", "pval.fixed.w", "zval.fixed.w")],  
+                     gROM[c("w.fixed.w")],NA, gROM[c("bylevs")]))
+  # gf$bylevs  <- str_c(gf$bylevs, "_f")
+  # 
+  # gr <- data.frame(c(gROM[c("bylevs", "TE.random.w", "seTE.random.w", 
+  #                               "lower.random.w", "upper.random.w", "statistic.random.w", "pval.random.w", "zval.random.w")], 
+  #                    NA,gROM[c("w.random.w")]))
+  # gr$bylevs  <- str_c(gr$bylevs, "_r")
+  ## assumed subgroup are fixed effect
+  
+  if (fixed$pval.fixed>=0.05) {
+    fixed$signif <- "ns"
+  } else {
+    fixed$signif <- ifelse(fixed$TE.fixed<0, "e", "c")
+  }
+  
+  if (random$pval.random>=0.05) {
+    random$signif <- "ns"
+  } else {
+    random$signif <- ifelse(random$TE.random<0, "e", "c")
+  }
+  stat <- NULL
+  signif <- NULL
+
+  for (i in 1:nrow(gf)) {
+    if (is.na(gf$pval.fixed.w[i])){
+      stat <- rbind(stat,"sub")
+      signif <- rbind(signif, ifelse(gf$TE.fixed.w[i]<0, "e", "c"))
+    } else if (gf$pval.fixed.w[i]>=0.05) {
+      stat <- rbind(stat, "sub")
+      signif <- rbind(signif, "ns")
+    } else {
+      stat <- rbind(stat, "sub")
+      signif <- rbind(signif, ifelse(gf$TE.fixed.w[i]<0, "e", "c"))
+    }
+  }
+  
+  gf$stat <- stat
+  gf$signif <- signif
+  
+  signif <- NULL
+  stat <- NULL
+  
+  for (i in 1:nrow(m)) {
+    if (is.na(m$pval[i])){
+      stat <- rbind(stat,"na")
+      signif <- rbind(signif, ifelse(m$TE[i]<0, "e", "c"))
+    } else if (m$pval[i]>=0.05) {
+      stat <- rbind(stat, "stat")
+      signif <- rbind(signif, "ns")
+    } else {
+      stat <- rbind(stat, "stat")
+      signif <- rbind(signif, ifelse(m$TE[i]<0, "e", "c"))
+    }
+  }
+  
+  m$stat <- stat
+  m$signif <- signif
+  
+  colnames(fixed) <- colnames(m)
+  colnames(random) <- colnames(m)
+  colnames(gf) <- colnames(m)
+  #colnames(gr) <- colnames(m)
+  
+  m <- rbind(m, gf, fixed, random)
+  m$stat <- factor(m$stat, levels = c('sum', 'sub', 'stat', 'na' ), order = TRUE)
+  m$signif <- factor(m$signif, levels = c('ns', 'e', 'c' ), order = TRUE)
+  m$subgroup <- factor(m$subgroup, levels = c('Obese', 'Colitis', 'IBS', 'Other_GI', 'Other', 'Healthy', 'FE', 'RE'), order = TRUE)
+  # m$signif <- factor(m$signif, levels = c("na.e", "na.c", "na.ns",
+  #                                     "stat.e", "stat.c", "stat.ns",
+  #                                     "sum.e", "sum.c", "sum.ns"), order=TRUE)
+  m <- m[order(m$subgroup),]
+  m$studlab <- factor(m$studlab, levels = m$studlab, order=TRUE)
+  
+  return(m)
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -135,19 +269,23 @@ color_factor <- cbind(c("na.e", "na.c", "na.ns",
                         c(1,1,1,16,16,16,18,18,18))
 colnames(color_factor) <- c("signif", "color", "shape")
 
-tROM_alpha_ACE<- left_join(tROM_alpha_ACE, color_factor, by = "signif", copy = TRUE)
 
 
-check_plot <- function(tROM){
-  ggplot(data = tROM, aes(x=TE, y=studlab, color = signif, shape = signif)) +
+check_plot <- function(tROM, filepath, width = w, height = h){
+  g<- ggplot(data = tROM, aes(x=TE, y=studlab, color = signif, shape = stat)) +
+    ggtitle(deparse(substitute(tROM)))+
     geom_vline(xintercept = 0, linetype="dashed", color="grey50") +
     geom_errorbarh(aes(xmin=lower, xmax=upper), height=0 ) +
-    theme_MicrobeR() +
-    geom_point()+
+    scale_color_manual(values=c( "gray44", "skyblue3", "pink3"))  +
+    geom_point(aes(size = stat))+
+    scale_shape_manual(values=c(18, 5, 16, 1))+
+    scale_size_manual(values=c(2.5, 2, 1, 0.5))+
     scale_y_discrete(limits=rev)+
     theme(panel.border = element_blank(), axis.line = element_line()) +
-    theme(axis.text.x=element_text(angle=45, hjust=1))
+    theme(axis.text.x=element_text(hjust=1))
+
 }
+check_plot(gtROM_ACE)
 
 
 
